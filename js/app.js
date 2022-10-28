ceece2c14b116bc19bad2e4abe6b9569c58eb2d4
James-benny <bennyapril696@gmail.com>
2022-10-28 16:42:33 +0100

    form

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let registrationForm = document.querySelector('#registry-form');
+let userName = document.querySelector('#name');
+let userEmail = document.querySelector('#email');
+let userPassword = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirmPassword');
+// let gender = document.querySelector('input [name="gender"]:checked');
+
+registrationForm.addEventListener('submit', createUser)
+
+function createUser(e) {
+    e.preventDefault()
+    console.log("first")
+
+    let userDetails = {
+        name: userName.value,
+        email: userEmail.value,
+        password: userPassword.value,
+        confirmPassword: confirmPassword.value,
+    };
+
+    if (
+        userDetails.name == "" && userDetails.email == "" && userDetails.password == "" 
+    ) {
+        alert("input your name");
+    }   else if(userDetails.userEmail === '') {
+        alert("add email")
+    }   else if(userDetails.password === '') {
+        alert("password");
+    }
+        else if ((userDetails.password !== userDetails.confirmPassword)) {
+            alert("password do not match!!");
+        } else {
+            console.log(userDetails);
+            window.location.href = 'home.html'
+        }
+}
